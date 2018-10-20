8918932ab94157366cc8c721a4d314ac983a635f
Aly <38168385+alycapps@users.noreply.github.com>
2018-10-20 12:17:56 -0400

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from "./components/card"
-import doctors from "./doctors.json"
+import Card from "./components/card";
+import doctors from "./doctors.json";
+import Navbar from "./components/navbar";
+import Jumbotron from "./components/jumbotron";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar />
+      <Jumbotron />
+      <br></br>
         <Card doctors={doctors} />
       </div>
     );
---
+++ src/components/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Jumbotron = () => (
+  <wrapper>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Doctor Who Memory</h1>
+        <h3>Take your time and try to click all of the Doctors without clicking a regeneration twice!</h3>
+        <p className="lead">"People assume that time is a strict progression of cause to effect, but actually, from a nonlinear, non-subjective viewpoint, it's more like a big ball of wibbly-wobbly, timey-wimey...stuff"</p>
+      </div>
+    </div>
+  </wrapper>
+);
+
+export default Jumbotron;

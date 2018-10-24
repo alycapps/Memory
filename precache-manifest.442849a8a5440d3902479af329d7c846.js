c6d902c6347a415c9bb8c00d1055b4a6bcda8e7a
Aly <38168385+alycapps@users.noreply.github.com>
2018-10-23 22:52:06 -0400

    Updates

---
+++ precache-manifest.442849a8a5440d3902479af329d7c846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee785b886b97e924cb97",
+    "url": "/Memory/static/js/runtime~main.ee785b88.js"
+  },
+  {
+    "revision": "403d0ae7725ed3d2440a",
+    "url": "/Memory/static/js/main.403d0ae7.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/Memory/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "403d0ae7725ed3d2440a",
+    "url": "/Memory/static/css/main.1c147248.chunk.css"
+  },
+  {
+    "revision": "08b7e285cb2142851a6a5255c41284cb",
+    "url": "/Memory/index.html"
+  }
+];
